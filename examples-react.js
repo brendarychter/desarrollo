f70c1254fdf4e2652e9cb9e459370e4340c8d63e
Brenda <brendarychter@gmail.com>
2023-10-03 14:12:14 -0300

    first commit

---
+++ examples-react.js
@@ -0,0 +1,8 @@
+// Ejemplo de un componente simple en React
+import React from 'react';
+
+function MiComponente() {
+  return <div>Hola, soy un componente de React</div>;
+}
+
+export default MiComponente;
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  // Usar esta porción de código como base
+  function getData () { // Definición de función
+    // Código de llamada a API
+  }
+
+  getData() // Llamada a la función
+
+  //
+
+  
+  return (
+    <>
+      <h3>Rick and Morty</h3>
+    </>
+  )
+}
+
+export default App
